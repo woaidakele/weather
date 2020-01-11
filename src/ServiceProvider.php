6b627f0026efb539f3e18ea0ac9c71197b056184
dakele <463228640@qq.com>
2020-01-11 09:51:51 +0800

    v1.0.3

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,22 +1,22 @@
 <?php
-
-namespace Dakele\Weather;
-
-class ServiceProvider extends \Illuminate\Support\ServiceProvider
-{
-    protected $defer = true;
-
-    public function register()
-    {
-        $this->app->singleton(Weather::class, function(){
-            return new Weather(config('services.weather.key'));
-        });
-
-        $this->app->alias([Weather::class, 'weather',Ipinfo::class, 'ipinfo']);
-    }
-
-    public function provides()
-    {
-        return [Weather::class, 'weather',Ipinfo::class, 'ipinfo'];
-    }
-}
+//
+//namespace Dakele\Weather;
+//
+//class ServiceProvider extends \Illuminate\Support\ServiceProvider
+//{
+//    protected $defer = true;
+//
+//    public function register()
+//    {
+//        $this->app->singleton(Weather::class, function(){
+//            return new Weather(config('services.weather.key'));
+//        });
+//
+//        $this->app->alias([Weather::class, 'weather',Ipinfo::class, 'ipinfo']);
+//    }
+//
+//    public function provides()
+//    {
+//        return [Weather::class, 'weather',Ipinfo::class, 'ipinfo'];
+//    }
+//}
